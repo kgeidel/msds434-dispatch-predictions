623a3d5d603b8ac573cb97158c79a88507a7d56e
Kevin Geidel <kgeidel@gmail.com>
2025-01-25 23:15:46 -0500

    reorg codebase

--- dispatch-predictions-app/django_init.py
+++ dispatch-predictions-app/django_init.py
@@ -3,7 +3,7 @@ from django import setup
 
 # derive location to your django project setting.py
 proj_path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
-proj_path = os.path.join(proj_path, "msds434-dispatch-predictions", "dispatch-predictions-app")
+proj_path = os.path.join(proj_path, "dispatch-predictions-app")
 # load your settings.py
 os.chdir(proj_path)
 
